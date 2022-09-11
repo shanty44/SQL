ca9affdbedad64fa36c1816690d5a1700c42c676
AGUSTIN IVIGLIA <80308615+shanty44@users.noreply.github.com>
2022-09-11 17:29:02 +0800

    Add files via upload

--- Script SENTENCIA SQL.sql
+++ Script SENTENCIA SQL.sql
@@ -1,4 +1,12 @@
-select b.nombre , c.nombre, d.nombre  , a.fechaadquisicion, f.nummatricula ,g.tipocolor , a.kms ,  e.empresa , a.poliza 
+select b.nombre as Modelo , 
+c.nombre as Marca,
+d.nombre as GrupoEmpresarial  , 
+a.fechaadquisicion as FechaAdquisicion, 
+f.nummatricula as Matricula ,
+g.tipocolor as Color ,
+a.kms ,  
+e.empresa,
+a.poliza 
 from trabajo_final_iviglia.coche a , trabajo_final_iviglia.modelo b, trabajo_final_iviglia.marca c, trabajo_final_iviglia.grupoempresarial d, trabajo_final_iviglia.seguro e, trabajo_final_iviglia.matricula f, trabajo_final_iviglia.color g
 where  activo = 'SI'
 and a.idmodelo  = b.idmodelo 
